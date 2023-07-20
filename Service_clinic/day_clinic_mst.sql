996d0d5590536199d85a33ca9f1c56c10ec40571
Sayantani <88243330+musicallysouled@users.noreply.github.com>
2023-07-20 11:47:43 +0530

    Update and rename day_clinic_mst.sql to 02Day_Clinic_Mst.sql

--- Service_clinic/day_clinic_mst.sql
+++
@@ -1,9 +0,0 @@
-﻿Create Table DAY_CLINIC_TYP_MST(
-DAY_CLINIC_TYP_CD nvarchar(5) Not Null,
-DAY_CLINIC_TYP_DESC nvarchar(100) Not Null,
-ACT_FLG char(1) default 'A' Not Null,	
-MOD_DT date Not Null,	
-
-Constraint PK001_M0389_0002 Primary Key  (DAY_CLINIC_TYP_CD),
-Constraint CK001_M0389_0002 Check (ACT_FLG in ('A','I','D'))
-);
