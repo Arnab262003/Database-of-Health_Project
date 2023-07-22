b096a5e50141f5f5cd90b985eb2d2756d15adcd1
Sayantani <88243330+musicallysouled@users.noreply.github.com>
2023-07-22 11:44:46 +0530

    Update StoredProcedure_Day_Clinic_Typ_Mst.sql

--- Service_clinic/StoredProcedure_Day_Clinic_Typ_Mst.sql
+++ Service_clinic/StoredProcedure_Day_Clinic_Typ_Mst.sql
@@ -1,9 +1,29 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Sayantani Dey
+-- Create date: 15-07-2023
+-- Description:	To insert, update, delete an entry from DAY_CLINIC_TYP_MST
+-- =============================================
 CREATE PROCEDURE [dbo].[Day_Clinic_Typ_Mst_Modify]
-	(@DAY_CLINIC_TYP_CD nvarchar,
-  @DAY_CLINIC_TYP_DESC nvarchar,
-  @ACT_FLG char = 'A' 	
-  @MOD_DT date = '1/1/1999',
-	@QueryType nvarchar(10) = 'Insert')
+	(@DAY_CLINIC_TYP_CD nvarchar(5),
+	  @DAY_CLINIC_TYP_DESC nvarchar(100),
+	  @ACT_FLG char(1) = 'A' ,	
+	  @MOD_DT Date,
+	  @QueryType nvarchar(10) = 'Insert')
 	AS
 	BEGIN
 		SET NOCOUNT ON;
